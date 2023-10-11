c2d2c6cc27bd8c210e968c31deafc84481a88cd0
Rubencito2002 <93660491+Rubencito2002@users.noreply.github.com>
2023-10-11 10:13:23 +0200

    Actualizado 11-10-23
    Se ha modificado el fichero rastrador.py en su codigo fuente y se esta desarrollando el readme.md para ir preparando su documentacion

--- README.md
+++ README.md
@@ -30,3 +30,30 @@ La temática de este proyecto es un rastrador de peliculas y series tv. Su funci
 Para ello se va utilizar para la recogidad de la informacion de peliculas y series tv una Api. 
 Cuya Api es: https://www.omdbapi.com/ (Para esta Api se necesita solicitar a la página una clave para poder tener acceso)
 
+##############################
+### Instalación Requeridad ###
+##############################
+
+Para poder usar esta aplicación dentras que tener instalado lo necesario que se indica en el fichero requirements.txt
+
+##############################################
+### Desarrollo del Código Fuente Principal ###
+##############################################
+
+Para poder desarrollar esta aplicacion se ha tenido que utilizar varias funciones para que se pueda desarrollar en condiciones el código fuente. 
+
+A la hora de ejecutarse nuestra aplicación la primera funcion que se pondra en funcionamiento es la de main que es la función que da la llamada a las otras funciones para que pueda realizar de desarrollo. Dentro de esta función lo que tenemos es una primera variable que la declaramos para guardar nuestra clave de nuestra API que vamos a utilizar, despues de esta declaracion realizaremos un bucle while para que nos muestre un menu para que el usuario pueda realizar una de las opcion que se muestra que son las siguientes:
+    Primero:  muestra un mensaje de menu de opciones.
+    Segundo: muestra la primera opcion que nos muestra uan descripcion que se va a realizar cuando se introduzca ese valor.
+    Tercero:  muestra la segunda opcion que nos muestra uan descripcion que se va a realizar cuando se introduzca ese valor.
+    Cuarto: muestra la tercera opcion que nos muestra uan descripcion que se va a realizar cuando se introduzca ese valor.
+    Quinto: se declara una variable con la que se le va asignar segun lo que introduzca el usuario.
+    Sexto: despues de la declaracion de la variable anterior comprobaremos que si en la variable cumple unas condiciones que indicara en las condiciones del if.
+    Septimo: dentro del if si se cumple la primera condicion se hara la llamada a la funcion de buscarPeliculaPorTitulo() sino tendra que volver a comprobar si se cumple la siguiente condicion que si la segunda condicion si la cumple hara la llamada a la funcion de buscarSeriePorTitulo() sino la cumple tampoco nos mostrara un mensaje de hasta luego y se acabara el programa. Si en la variable no se ha introducido un valor permitido nos mostrara un mensaje que solo puede introducir ciertos valores.
+
+La funcion main() es llamada a traves de un if que nos mostrara un mensaje de bienvenida y hara la llamada a la funcion para que realize todo lo anterior que se ha explicado.
+
+#########################################################################
+### Desarrollo de la funciones de buscar pelicula o series por titulo ###
+#########################################################################
+
--- rastreador.py
+++ rastreador.py
@@ -4,61 +4,70 @@ import json
 
 # Creamos una funcion para buscar las peliculas por su titulo.
 def buscarPeliculaPorTitulo(api_key):
-    busqueda = input("Introduce el título de la película o serie: ")
+    busqueda = input("\nIntroduce el título de la película: ")
     url = f"http://www.omdbapi.com/?apikey={api_key}&t={busqueda}"
-    response = requests.get(url)
+    response = requests.get(url) # Usando la url de la API lo que hace la peticion con lo que el usuario ha solicitado.
 
     if response.status_code == 200: # Es para comprobar si se ha conectado a la API cuando se ha hecho la llamada anteriomente.
         datos = json.loads(response.text)
         if datos.get("Response") == "True": # Si en los datos cargados existe la informacion que se solicita pues nos mostrara lo solicitado
-            print("Información de la película o serie:")
-            print(f"Título: {datos['Title']}")
-            print(f"Año: {datos['Year']}")
+            print("\nInformación de la pelicula:")
+            print(f"Title: {datos['Title']}")
+            print(f"Released: {datos['Released']}")
+            print(f"Year: {datos['Year']}")
             # Para comprobar si hay informacion sobre el director preguntamos antes de mostrarlo si hay datos.
             if datos.get("Director") == "":
                 print(f"Director: {datos['Director']}")
             else:
                 print(f"Director: No hay informacion")
-            print(f"Actores: {datos['Actors']}")
+            print(f"Genre: {datos['Genre']}")
+            print(f"Actors: {datos['Actors']}")
             print(f"Sinopsis: {datos['Plot']}")
+            print(f"Language: {datos['Language']}")
+            print(f"Country: {datos['Country']}")
             # Para comprobar si hay informacion sobre si ha sido premiado preguntamos antes de mostrarlo si hay datos.
             if datos.get("Awards") == "":
-                print(f"Premiado: {datos['Awards']}")
+                print(f"Awards: {datos['Awards']}")
             else:
-                print(f"Premiado: No ha sido premiado todavia")
+                print(f"Awards: Has not been awarded yet")
+            print(f"Taquilla: {datos['BoxOffice']}")
         else:
-            print("Película o serie no encontrada.")
+            print("Movie not found.")
     else:
-        print("Error al conectarse a la API.")
+        print("Error connecting to API.")
 
 # Creamos una funcion para buscar las series por su titulo.
 def buscarSeriePorTitulo(api_key):
-    busqueda = input("Introduce el título de la serie: ")
+    busqueda = input("\nIntroduce el título de la serie: ")
     url = f"http://www.omdbapi.com/?apikey={api_key}&t={busqueda}"
-    response = requests.get(url)
+    response = requests.get(url) # Usando la url de la API lo que hace la peticion con lo que el usuario ha solicitado.
 
     if response.status_code == 200: # Es para comprobar si se ha conectado a la API cuando se ha hecho la llamada anteriomente.
         datos = json.loads(response.text)
-        if datos.get("Response") == "True": # Si en los datos cargados existe la informacion que se solicita pues nos mostrara lo solicitado
-            print("Información de la serie:")
-            print(f"Título: {datos['Title']}")
-            print(f"Año: {datos['Year']}")
+        if datos.get("Response") == "True": # Si en los datos cargados existe la informacion que se solicita pues nos mostrara lo solicitado.
+            print("\nInformación de la serie:")
+            print(f"Title: {datos['Title']}")
+            print(f"Released: {datos['Released']}")
+            print(f"Year: {datos['Year']}")
             # Para comprobar si hay informacion sobre el director preguntamos antes de mostrarlo si hay datos.
             if datos.get("Director") == "":
                 print(f"Director: {datos['Director']}")
             else:
                 print(f"Director: No hay informacion")
-            print(f"Actores: {datos['Actors']}")
+            print(f"Genre: {datos['Genre']}")
+            print(f"Actors: {datos['Actors']}")
             print(f"Sinopsis: {datos['Plot']}")
+            print(f"Language: {datos['Language']}")
+            print(f"Country: {datos['Country']}")
             # Para comprobar si hay informacion sobre si ha sido premiado preguntamos antes de mostrarlo si hay datos.
             if datos.get("Awards") == "":
-                print(f"Premiado: {datos['Awards']}")
+                print(f"Awards: {datos['Awards']}")
             else:
-                print(f"Premiado: No ha sido premiado todavia")
+                print(f"Awards: Has not been awarded yet")
         else:
-            print("Serie no encontrada.")
+            print("Series not found.")
     else:
-        print("Error al conectarse a la API.")
+        print("Error connecting to API.") # Mensaje de Error por si no se ha podido conectar a la API.
 
 def main():
     # Creamos una variable que se le va asignar nuestra clave para conectarno a la API para poder hacer nuestras busquedas.
@@ -82,4 +91,5 @@ def main():
             print("Opción no válida. Introduce 1 , 2 o 3.")
 
 if __name__ == "__main__":
+    print("Bienvenido al Rastreador de Películas y Series de TV")
     main()
